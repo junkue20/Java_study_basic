fad9875f680b1a4170bb9662a1294ad3857ba2cc
junkue20 <junkue17@naver.com>
2023-01-31 17:50:43 +0900

    11

--- java_20230130/src/cls/Member.java
+++ java_20230130/src/cls/Member.java
@@ -26,7 +26,7 @@ public class Member {
 	// 2. 반환타입 문자(String)로 반환해야 함.
 	// 3. 메소드명 (Main4에서 전달한 값)
 	// public 는 보내는값, public void는 가져오는 값
-	// 메소드는 통로역활
+	// 메소드는 통로역할
 	public String getUserId() {
 		return userId;
 	}
