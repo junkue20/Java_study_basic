1d2b18849a2f1898f762201881a40ed5ab9467b7
junkue20 <junkue17@naver.com>
2023-01-31 16:46:14 +0900

    수정

--- java_20230130/src/cls/KorScore.java
+++ java_20230130/src/cls/KorScore.java
@@ -13,12 +13,55 @@ public class KorScore {
 	private String[] student = new String[5];
 	private int[] score = new int[5];
 
-	//학생들의 점수 합계를 구해주는 메소드
+	// 학생들의 점수 합계를 구해주는 메소드
 	public int sumScore() {
-	 int sum = 0;
-	 
-	 return sum;
+		int sum = 0;
+		int i;
+		for (i = 0; i < score.length; i++) { // 0 1 2 3 4
+			sum = sum + this.score[i];
+		}
+		return sum;
 	}
-	// setter, getter, toString 메소드가 존재함
-	
+
+	public float avgScore() {
+		float avg = 0.0f;
+		float sum = this.sumScore();
+		avg = sum / (float) this.score.length; // 실수 한개이상 필요
+		return avg;
+	}
+
+	public int Score90() {
+		int ctl = 0;
+		int i = 0;
+		for (i = 0; i < this.score.length; i++) {
+			if (this.score[i] >= 90) {
+				ctl = ctl + 1;
+			}
+		}
+		return ctl;
+
+	}
+
+	public int scoreMax() {
+		int i;
+		int max = this.score[0]; // 맨 앞의 값인 20이 들어가게 됨.
+		for (i = 0; i < this.score.length; i++) {
+			if (max < this.score[i]) {
+				max = this.score[i];
+			}
+		}
+		return max;
+	}
+
+	public int scoreMin() {
+		int i;
+		int min = this.score[0]; // 맨 앞의 값인 20이 들어가게 됨.
+		for (i = 0; i < this.score.length; i++) {
+			if (min > this.score[i]) {
+				min = this.score[i];
+			}
+		}
+		return min;
+		// setter, getter, toString 메소드가 존재함
+	}
 }
--- java_20230130/src/main1/Main7.java
+++ java_20230130/src/main1/Main7.java
@@ -8,25 +8,30 @@ public class Main7 {
 		// 1. 사용자가 입력한 값
 		String[] s1 = { "가나다", "마바사", "아자차", "타파하" };
 		int[] s2 = { 20, 99, 70, 98, 100 };
-		
+
 		// 2. KorScore 클래스로 계산해야하는 데이터를 전송 (setter)
 		KorScore obj = new KorScore();
 		obj.setStudent(s1);
 		obj.setScore(s2);
-		
+
 		// 3). 전체 합계를 구하기
-		
+		int sum = obj.sumScore();
+		System.out.println("점수합은 = " + sum);
 		// 4. 평균
-		
+		float avg = obj.avgScore();
+		System.out.println("평균점수는 = " + avg);
 		// 5. 최대점수
-		
+		int max = obj.scoreMax();
+		System.out.println("최대값은 = " + max);
 		// 6. 최소점수
-		
+		int min = obj.scoreMin();
+		System.out.println("최소값은 = " + min);
 		// 7. 90점 이상인 사람이 몇명인지
-		
-		
+		int ctl = obj.Score90();
+		System.out.println("90점 이상인 사람의 수는 = " + ctl);
 		// 8. 확인
 		System.out.println(obj.toString());
+
 	}
 
 }
