9b11b6db02c0bb5cdd7fb2ea80400ffffae36470
junkue20 <junkue17@naver.com>
2023-02-02 12:45:36 +0900

    1
    1

---
+++ java_20230130/src/day4/AdressMain.java
@@ -0,0 +1,22 @@
+package day4;
+
+import java.util.Date;
+
+public class AdressMain {
+
+	public static void main(String[] args) {
+
+		
+		MemberTable obj = new MemberTable("id", "Pw", "123준규", 
+				"010-1234-1233", "C", new Date());
+		
+		AddressTable address = new AddressTable(1000, "busan", "1234", new Date(), obj);
+		//값 입력받기
+		
+		// 우편번호 5자리 확인
+		
+		
+		System.out.println(address.showMonth());
+	}
+
+}
