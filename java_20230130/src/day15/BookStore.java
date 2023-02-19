80b843e647ee7138db9bb74d5fccf94db05eecb9
junkue20 <junkue17@naver.com>
2023-02-19 19:03:49 +0900

    2/19복습 추가

--- java_20230130/src/day15/BookStore.java
+++ java_20230130/src/day15/BookStore.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bson.Document;
-
 import lombok.Data;
 
 // 타입
--- java_20230130/src/review/Main.java
+++ java_20230130/src/review/Main.java
@@ -1,21 +1,21 @@
 package review;
 
 import java.util.Scanner;
-
-// 킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int piece;
-		int[] chessArray = { 1, 1, 2, 2, 2, 8 };
-		int[] Answer = new int[6];
-
-		for (int i = 0; i < chessArray.length; i++) {
-			piece = sc.nextInt();
-			Answer[i] = chessArray[i] - piece;
-			System.out.print(Answer[i] + " ");
-		}
-		
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		int sumM = sc.nextInt();
+		int sumH = (M + sumM) / 60;
+		if ((M + sumM) >= 60) { // 만일 분이 60분을 넘어가게되면
+			H = (H + sumH); // 추가된만큼 시간 변경
+			M = (M + sumM) % 60; // 나머지 분 설정
+			if ((H + sumH) >= 24) { // if 24시가 넘어간 시간? 
+				H = (H + sumH) - 25; // 
+			} 
+			System.out.println(H + " " + M);
+		} else
+			System.out.println((H + sumH) + " " + (M + sumM));
 	}
 }
