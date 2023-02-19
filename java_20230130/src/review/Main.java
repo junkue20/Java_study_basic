9f8971e7b36f3043da125b46a84a92794eb0a4f5
junkue20 <junkue17@naver.com>
2023-02-20 00:45:37 +0900

    Update Main.java

--- java_20230130/src/review/Main.java
+++ java_20230130/src/review/Main.java
@@ -8,11 +8,12 @@ public class Main {
 		int M = sc.nextInt();
 		int sumM = sc.nextInt();
 		int sumH = (M + sumM) / 60;
+		
 		if ((M + sumM) >= 60) { // 만일 분이 60분을 넘어가게되면
 			H = (H + sumH); // 추가된만큼 시간 변경
 			M = (M + sumM) % 60; // 나머지 분 설정
 			if ((H + sumH) >= 24) { // if 24시가 넘어간 시간? 
-				H = (H + sumH) - 25; // 
+				H -= 24; // 
 			} 
 			System.out.println(H + " " + M);
 		} else
