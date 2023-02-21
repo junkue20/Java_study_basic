176e2c87bd53e11b550bf5742725ca61f04ebfbf
junkue20 <junkue17@naver.com>
2023-02-21 18:35:11 +0900

    0221 수업완료

--- java_20230130/src/review/Main.java
+++ java_20230130/src/review/Main.java
@@ -1,22 +1,15 @@
 package review;
 
-
 import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int H = sc.nextInt();
-		int M = sc.nextInt();
-		int sumM = sc.nextInt();
-		int sumH = (M + sumM) / 60;
-		if ((M + sumM) >= 60) { // 만일 분이 60분을 넘어가게되면
-			H = (H + sumH); // 추가된만큼 시간 변경
-			M = (M + sumM) % 60; // 나머지 분 설정
-			if ((H + sumH) >= 24) { // if 24시가 넘어간 시간? 
-				H -= 24; // 
-			} 
-			System.out.println(H + " " + M);
-		} else
-			System.out.println((H + sumH) + " " + (M + sumM));
+		int num = sc.nextInt(); 
+		for(int i = 1; i<=num; i++) {
+			int A = sc.nextInt(); 
+			int B = sc.nextInt(); 
+			System.out.println(A+B);
+		}
 	}
 }
