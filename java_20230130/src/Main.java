752c6cba13563038c7d936cfad1eae23ce305f23
junkue20 <junkue17@naver.com>
2023-02-27 18:09:45 +0900

    백준코드 수정

--- java_20230130/src/Main.java
+++ java_20230130/src/Main.java
@@ -4,17 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		while (1 > 0) {
+		while (sc.hasNextInt()) {
 			int A = sc.nextInt();
 			int B = sc.nextInt();
-			if (A == 0 && B == 0) {
-				break;
-			}
-			int sum = A + B;
-			System.out.println(sum);
-		}
 
+			System.out.print(A + B);
+
+		}
 	}
 
 }
