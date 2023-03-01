1c2db1215e70f3c22a513aeb8e0dc5a5c14c0f0f
junkue20 <junkue17@naver.com>
2023-03-02 01:59:14 +0900

    백준 배열문제 해결

--- backpract/src/Main.java
+++ backpract/src/Main.java
@@ -1,16 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNextInt()) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
+	public static void main(String[] args) throws IOException { // 버퍼리더 사용시 반드시 예외처리 필요
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str = new StringTokenizer(br.readLine(), " "); // 입력받은 값에서 빈칸을 구분함.
 
-			System.out.print(A + B);
-			System.out.println();
+		int N = Integer.parseInt(str.nextToken());
+		int X = Integer.parseInt(str.nextToken()); // 차례대로 수열의 길이값 N, X 입력
+
+		str = new StringTokenizer(br.readLine(), " "); // 새로운 값 입력
+		
+		int[] A = new int[N];
+		for (int i = 0; i < A.length; i++) {
+			A[i] = Integer.parseInt(str.nextToken()); // 수열 A에 들어가는 값들
+			if (X > A[i]) {
+				System.out.print(A[i] + " "); // A에서 X보다 작은값들 출력. 
+			}
 		}
 	}
-
 }
