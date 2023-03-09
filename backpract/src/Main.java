c2abc1131417aa3804337ff983827410fad27524
junkue20 <junkue17@naver.com>
2023-03-09 20:08:55 +0900

    Update Main.java
    백준 배열 3052번

--- backpract/src/Main.java
+++ backpract/src/Main.java
@@ -1,41 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer str = new StringTokenizer(br.readLine(), " ");
 
-		int[] A = new int[30];
-		for (int i = 0; i < A.length; i++) {
-			A[i] = i + 1;
-		}
-
-		int[] B = new int[28];
-		for (int i = 0; i < B.length-1; i++) {
-			B[i] = Integer.parseInt(str.nextToken());
-			if(i < 27) {
-				str = new StringTokenizer(br.readLine(), " ");
-			}
-		}
+		int[] A = new int[10];
+		int cnt = 0;
 
 		for (int i = 0; i < A.length; i++) {
-			for (int j = 0; j < B.length-1; i++) {
-				if (A[i] == B[j]) {
-					A[i] = 0;
+			A[i] = Integer.parseInt(br.readLine()) % 42;
+			if (i == 9) {
+				for (int j = 0; j < A.length; j++) {
+					if (A[i] != A[j]) {
+						cnt++;
+					}
 				}
 
 			}
 		}
-		for (int i = 0; i < A.length; i++) {
-			if (A[i] != 0) {
-				System.out.println(A[i]);
-			}
-
-		}
+		System.out.println(cnt);
 
 	}
 }
