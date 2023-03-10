2b1ebe6d00c265e3b9136e4896a03f548d216e45
junkue20 <junkue17@naver.com>
2023-03-10 09:40:53 +0900

    1
    1

--- backpract/src/Main.java
+++ backpract/src/Main.java
@@ -1,31 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException { // 버퍼리더 사용시 반드시 예외처리 필요
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer str = new StringTokenizer(br.readLine(), " "); // 입력받은 값에서 빈칸을 구분함.
+//		StringTokenizer str = new StringTokenizer(br.readLine(), " "); // 입력받은 값에서 빈칸을 구분함.
+//		str = new StringTokenizer(br.readLine(), " "); // 새로운 값 입력
 
-		long[] A = new long[9]; // 배열 총 길이, 9칸
+		int[] A = new int[10];
+		int cnt = 0;
+		boolean bl;
 
-		str = new StringTokenizer(br.readLine(), " "); // 새로운 값 입력
-
-		for (int i = 0; i < A.length; i++) {
-			A[i] = Integer.parseInt(str.nextToken()); // 수열 A에 들어가는 값들
+		for (int i = 0; i < 10; i++) {
+			A[i] = Integer.parseInt(br.readLine()) % 42; // 10개의 정수값 입력
 		}
-
-		long max = 0;
-		for (int i = 0; i < A.length; i++) {
-			if (max < A[i]) {
-				max = A[i];
-				if (max == A[i]) {
-					System.out.println(max);
-					System.out.println(A[i]);
+		for (int i = 0; i < 10; i++) {
+			bl = true; // bl값 초기화.
+			for (int j = i + 1; j < A.length; j++) {
+				if (A[i] == A[j]) { // 조건 : 서로 다른 값이 몇개 있는지 출력 ex) [0,1,0,0,0,2,3,4,3,2]
+					bl = false;  
+					break; // 비교하려는 A[i]와 같은수가 배열에 존재할 시 반복문 stop. (+ bl값은 cnt의 카운트용.)
 				}
 			}
+			if (bl == true) {
+				cnt++;
+			}
 		}
+		System.out.println(cnt);
 	}
+
 }
