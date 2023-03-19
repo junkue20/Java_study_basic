9b21bd5fadb0789e33043ec61907edee590e7dad
junkue20 <junkue17@naver.com>
2023-03-19 21:24:20 +0900

    필요없는 부분 일괄삭제

--- backpract/src/Main.java
+++ backpract/src/Main.java
@@ -1,31 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer str = new StringTokenizer(br.readLine(), " ");
-
-		int n = Integer.getInteger(str.nextToken()); // 배열 길이 첫줄 입력
-		int[] arr = new int[n];
-
-		str = new StringTokenizer(br.readLine(), " "); // 입력값 초기화 
-
-		for (int i = 0; i < arr.length; i++) { // 과목 점수 입력
-			arr[i] = Integer.getInteger(str.nextToken()); // 이후에 첫번째 행렬에 3개가 한꺼번에 들어가는 문제가 발생할 수 있음.
-		}
-		
-		int max = 0;
-		for (int i = 0; i< arr.length; i++) { // 최대값 
-			if(max < arr[i]) {
-				max = arr[i];
-			}
-		}
-		
-		
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer str = new StringTokenizer(br.readLine());
+        
+       int T = Integer.parseInt(br.readLine());
+       
+       for(int i = 0; i<T; i++) {
+    	   String str = br.readLine();
+    	   System.out.println(str.charAt(0) + str.charAt(str.length()-1) + "\n"); 
+       }
+        
+    }
 }
