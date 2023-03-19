8194ee79850a99d3c1e12e5cc99e88edcbbeccba
junkue20 <junkue17@naver.com>
2023-03-20 08:43:31 +0900

    1
    1

--- backpract/src/Main.java
+++ backpract/src/Main.java
@@ -1,31 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer str = new StringTokenizer(br.readLine(), " ");
 
-		int n = Integer.getInteger(str.nextToken()); // 배열 길이 첫줄 입력
-		int[] arr = new int[n];
+		int num = Integer.parseInt(br.readLine());
 
-		str = new StringTokenizer(br.readLine(), " "); // 입력값 초기화 
-
-		for (int i = 0; i < arr.length; i++) { // 과목 점수 입력
-			arr[i] = Integer.getInteger(str.nextToken()); // 이후에 첫번째 행렬에 3개가 한꺼번에 들어가는 문제가 발생할 수 있음.
+		String str = "";
+		for (int i = 1; i <= num; i++) {
+			str = br.readLine();
+			System.out.println(str.substring(1) + str.substring(str.length()));
 		}
-		
-		int max = 0;
-		for (int i = 0; i< arr.length; i++) { // 최대값 
-			if(max < arr[i]) {
-				max = arr[i];
-			}
-		}
-		
-		
-		
+
 	}
 }
