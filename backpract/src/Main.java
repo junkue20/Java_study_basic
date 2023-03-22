807768fc05a6207e1a7b60dced754a6b8be0f07d
junkue20 <junkue17@naver.com>
2023-03-22 16:21:50 +0900

    백준문제

--- backpract/src/Main.java
+++ backpract/src/Main.java
@@ -7,15 +7,29 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		// 사탕봉지 갯수
 		int n = Integer.parseInt(br.readLine());
-		String str = br.readLine();
+		int cnt = 0;
 
-		int sum = 0;
-		for (int y = 0; y <n; y++) {
-			sum += str.charAt(y)-'0';
+		// 5키로로 담을 수 있는만큼 담기.
+		cnt += n / 5;
+
+		// 만약 3으로 나눌 수 있는 수라면
+		if ((n % 5) % 3 == 0) {
+			cnt = (n % 5) / 3;
 		}
-		System.out.println(sum);
+
+		// 5키로를 담고 남는 나머지는 3키로 봉지로 담기.
+		if ((n % 5) != 0) {
+			cnt += (n % 5) / 3;
+
+			// 만약 5와 3으로 나누어담을 수 없다면 -1을 출력하도록 설정
+			if ((n % 5) % 3 != 0) {
+				cnt = (-1);
+			}
+		}
+		System.out.println(cnt);
+
 	}
 }
